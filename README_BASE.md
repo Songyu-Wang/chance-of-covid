fb4c9c4aa6e51c21ac17ee43949a43ee85c8982c
Songyu-Wang <wangsongyuf@gmail.com>
2020-06-23 23:28:50 -0700

    update

---
+++ README_BASE.md
@@ -0,0 +1,19 @@
+# What are the chance of encountering people with COVID if I go out today?
+I do not know, but I would like to know
+
+## Basic idea
+Calculate the percentage of people may have COVID today in state population
+
+### How can I find out how many people may have COVID today?
+Please note this is an over simplified educated guess. Do not over interpret the result 
+1. Using API https://covidtracking.com/ to get new case increase counts for the last 14 days for each state
+2. Find the maximum increase count in the past 14 days
+3. `Max value * 14 * 2` (This is by no means accurate. Too many factors are involved. I hope this is a conservative educated guess by using max value and `* 2` as fault tolerance mitigators ) 
+
+### How can I find out population in each state?
+https://www.census.gov/data/developers/data-sets/popest-popproj/popest.html
+
+## Note
+This project is under MIT license. I created it for personal reference. Feel free to do whatever you want with it. However, I am not responsible for any outcome regardless how you use or not use those data 
+
+## Result
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import us
 import requests
 import ast
+import arrow
 COVID_API = "https://covidtracking.com/api/v1/states/{}/daily.csv"
 POP_API = "https://api.census.gov/data/2019/pep/population?get=POP&for=state:*"
 LIST_OF_STATES_WITH_FIPS = {
@@ -28,12 +29,12 @@ for fips, state in LIST_OF_STATES_WITH_FIPS.items():
     guess_current_covid_count = max_in_past_14_days * 14 * 2
 
     prep_data['State'].append(us.states.lookup(state).name)
-    prep_data['Possibility_1'].append(
-        guess_current_covid_count / pop_data[state])
-    prep_data['Possibility_10'].append(
-        guess_current_covid_count / pop_data[state] * 10)
-    prep_data['Possibility_100'].append(
-        guess_current_covid_count / pop_data[state] * 100)
+    prep_data['Possibility_1'].append("{:.5%}".format(
+        guess_current_covid_count / pop_data[state]))
+    prep_data['Possibility_10'].append("{:.5%}".format(
+        guess_current_covid_count / pop_data[state] * 10))
+    prep_data['Possibility_100'].append("{:.5%}".format(
+        guess_current_covid_count / pop_data[state] * 100))
     prep_data['Max'].append(max_in_past_14_days)
     prep_data['Estimated_existing'].append(guess_current_covid_count)
 
@@ -47,8 +48,12 @@ internal_to_external_name_map = {
     'Estimated_existing': 'Estimated people count with COVID'
 }
 df = df.rename(internal_to_external_name_map, axis='columns')
-df.to_html(open('data.html', 'w'), formatters={
-    internal_to_external_name_map['Possibility_1']: '{:,.2%}'.format,
-    internal_to_external_name_map['Possibility_10']: '{:,.2%}'.format,
-    internal_to_external_name_map['Possibility_100']: '{:,.2%}'.format,
-}, index=False)
+
+with open("README_BASE.md") as b:
+    with open("README.md", "w") as f:
+        for line in b:
+            f.write(line)
+        f.write('\n Updated at:' + str(arrow.utcnow())+'\n\n')
+df.to_markdown(open('README.md', 'a'),showindex=False)
+
+
